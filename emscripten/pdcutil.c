6ce5600e9813c0452f68e37f5427c8eea48e1c00
Boris Gjenero <boris.gjenero@gmail.com>
2017-03-13 20:03:54 -0400

    Implement PDC_napms() using emscripten_sleep(), make firework work
    Firework demo is now compiled using Emterpreter-Async, so that
    the synchronous sleeps work and the display is updated during them.
    All users of napms() must use Emterpreter-Async. For more info,
    see https://github.com/kripken/emscripten/wiki/Emterpreter

--- emscripten/pdcutil.c
+++ emscripten/pdcutil.c
@@ -1,5 +1,7 @@
 /* Public Domain Curses */
 
+#include <emscripten.h>
+
 #include "pdcemscripten.h"
 
 RCSID("$Id: pdcutil.c,v 1.6 2008/07/14 04:24:52 wmcbrine Exp $")
@@ -12,6 +14,13 @@ void PDC_beep(void)
 
 void PDC_napms(int ms)
 {
+    /* This is a synchronous sleep, which isn't normally possible with
+     * Emscripten. Code using it must be compiled with -s EMTERPRETIFY=1
+     * -s EMTERPRETIFY_ASYNC=1, and all functions in the call graph leading
+     * to this sleep must use the Emterpreter. For more information, see:
+     * https://github.com/kripken/emscripten/wiki/Emterpreter
+     */
+    emscripten_sleep(ms);
     PDC_LOG(("PDC_napms() - called: ms=%d\n", ms));
 }
 
